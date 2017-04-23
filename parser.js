721f65a1ac3b0bdb6b312e446c7e146ae077de8f
Geoff Colman <gcolman@lion.lmu.edu>
2017-04-23 15:58:18 -0700

    Some fixes for parsing grammar

--- parser.js
+++ parser.js
@@ -1,10 +1,11 @@
-const Program = require('./entities/Program.js');
+const Program = require('./entities/program.js');
 const Block = require('./entities/Block.js');
 const IfStatement = require('./entities/IfStatement.js');
 const IfElseStatement = require('./entities/IfElseStatement.js');
 const ElseIfStatement = require('./entities/ElseIfStatement.js');
 const ForStatement = require('./entities/ForStatement.js');
 const WhileStatement = require('./entities/WhileStatement.js');
+const PrintStatement = require('./entities/Print.js');
 const MatchStatement = require('./entities/MatchStatement.js');
 const ReturnStatement = require('./entities/ReturnStatement.js');
 const VariableDecl = require('./entities/VariableDecl.js');
@@ -14,6 +15,7 @@ const ArrayConstDecl = require('./entities/ArrayConstDecl.js');
 const IdExpression = require('./entities/IdExpression.js');
 const BinaryExpression = require('./entities/BinaryExpression.js');
 const UnaryExpression = require('./entities/UnaryExpression.js');
+const Literal = require('./entities/literal.js');
 const FloatLiteral = require('./entities/FloatLiteral.js');
 const StringLiteral = require('./entities/StringLiteral.js');
 const BooleanLiteral = require('./entities/BooleanLiteral.js');
@@ -32,6 +34,9 @@ const semantics = grammar.createSemantics().addOperation('ast', {
   Block(stmt1, _1, stmt2) {
     return new Block(stmt1.ast(), stmt2.ast());
   },
+  // IfStmt(i, con1, brac1, block1, brac2, elsi, con2, brac3, block2, brac4, els, brac5, block3, brac6) {
+  //   return new IfStatement(con1.ast(), block1.ast(), con2.ast(), block2.ast(), block3.ast());
+  // },
   IfStmt(i, con1, brac1, block1, brac2, elsi, con2, brac3, block2, brac4, els, brac5, block3, brac6) {
     return new IfStatement(con1.ast(), block1.ast(), con2.ast(), block2.ast(), block3.ast());
   },
@@ -46,6 +51,9 @@ const semantics = grammar.createSemantics().addOperation('ast', {
   },
   WhileStmt(w, e, l, b, r) {
     return new WhileStatement(e.ast(), b.ast());
+  },
+  Print(b, lp, arg, rp) {
+    return new PrintStatement(arg.ast());
   },
   MatchStmt(m, e1, w, _nl, matchpart) {
     return new MatchStatement(e1.ast(), matchpart.ast());
@@ -65,24 +73,28 @@ const semantics = grammar.createSemantics().addOperation('ast', {
   ConstArrayDecl(s, id, eq, brac1, v, comma1, nextv, brac2) {
     return new ArrayConstDecl(v.sourceString, nextv.sourceString);
   },
-  Exp(e1, op, e2) {
+  Exp_or(e1, op, e2) {
     return new BinaryExpression(e1.ast(), op.sourceString, e2.ast());
   },
-  Exp1(e1, op, e2) {
+  Exp_and(e1, op, e2) {
     return new BinaryExpression(e1.ast(), op.sourceString, e2.ast());
   },
-  Exp2(e1, op, e2) {
+  Exp1_relop(e1, op, e2) {
     return new BinaryExpression(e1.ast(), op.sourceString, e2.ast());
   },
-  Exp3(e1, op, e2) {
+  Exp2_addop(e1, op, e2) {
     return new BinaryExpression(e1.ast(), op.sourceString, e2.ast());
   },
-  Exp4(op, e) {
+  Exp3_mulop(e1, op, e2) {
+    return new BinaryExpression(e1.ast(), op.sourceString, e2.ast());
+  },
+  Exp4_negop(op, e) {
     return new UnaryExpression('-', e.ast());
   },
   Exp5_parens(left, e, right) {
     return e.ast();
   },
+
   // little confused on these ones
   id(idValue) {
     return new IdExpression(this.sourceString);
