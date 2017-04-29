3010056c72ae02072a812711b0668873a52775c0
Mike McAuley <mmcaule1@lion.lmu.edu>
2017-04-29 16:53:53 -0700

    pulling again

--- test/parser-test.js
+++ test/parser-test.js
@@ -5,14 +5,14 @@ describe('Parser Test', () => {
   describe('simple program', () => {
     it('hello world program', () => {
       const ast = parse('burp("HelloWorld")').toString();
-      const expected = '(Program  (Block (Print burp (StringLiteral "HelloWorld" ))))';
+      const expected = '(Program  (Block (Statement (Print burp (StringLiteral "HelloWorld" )))))';
       assert.equal(ast, expected);
     });
   });
   describe('simple binexp', () => {
     it('binexp', () => {
       const ast = parse('1 + 1').toString();
-      const expected = '(Program  (Block (BinaryExpression (FloatLiteral 1.0) + (FloatLiteral 1.0))))';
+      const expected = '(Program  (Block (Statement (BinaryExpression (FloatLiteral 1.0) + (FloatLiteral 1.0)))))';
       assert.equal(ast, expected);
     });
   });
