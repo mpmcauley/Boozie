629688cca577544bf67a10b21e0e6f74c9aef0dd
Justin Lee <jlee201@lion.lmu.edu>
2017-05-01 14:40:26 -0700

    trying to fix parser test

--- test/parser-test.js
+++ test/parser-test.js
@@ -44,29 +44,6 @@ describe('Parser Test', () => {
         const expected = '(Program (Block (VariableDecl ((IdExpression x) = (BoozieArray (FloatLiteral 7.0)))))';
         assert.equal(ast, expected);
       });
-// <<<<<<< HEAD
-    });
-  describe('if statement', () => {
-    it('solo if statement', () => {
-      const ast = parse('if x == y { burp("HelloWorld") }').toString();
-      const expected = '(Program (Block (IfStatement if (BinaryExpression (IdExpression x) == (IdExpression y)) { (Block (Print burp (StringLiteral "HelloWorld" ))) } )))';
-      assert.equal(ast, expected);
-    });
-  });
-
-  describe('return statement', () => {
-    it('return statement', () => {
-      const ast = parse('return 6').toString();
-      const expected = '(Program (Block (ReturnStatement return (FloatLiteral 6.0))))';
-      assert.equal(ast, expected);
-    });
-  });
-
-  describe('if else statement', () => {
-    it(' if else statement', () => {
-      const ast = parse('if x == y { burp("HelloWorld") } else { burp("Justin") }').toString();
-      const expected = '-(Program  (Block (IfStatement if (BinaryExpression (IdExpression x) == (IdExpression y)) { (Block (Print burp (StringLiteral "HelloWorld" ))) } )))';
-// =======
       it('another simple array', () => {
         const ast = parse('let x = [7, 8]').toString();
         const expected = '(Program (Block (VariableDecl ((IdExpression x) = (BoozieArray (FloatLiteral 7.0),(FloatLiteral 8.0)))))';
@@ -97,7 +74,6 @@ describe('Parser Test', () => {
     it('return statement', () => {
       const ast = parse('return 6').toString();
       const expected = '(Program (Block (ReturnStatement return (FloatLiteral 6.0))))';
-// >>>>>>> a36ffaac9cb7d747952f5456998e814fa8c818be
       assert.equal(ast, expected);
     });
   });
@@ -105,22 +81,7 @@ describe('Parser Test', () => {
   describe('for statement', () => {
     it('for statement', () => {
       const ast = parse('for beer in beers { burp("Khiem likes ping pong") }').toString();
-// <<<<<<< HEAD
-      const expected = '(Program (Block (ForStatement for (IdExpression beer) in (IdExpression beers) { (Block (Print burp (StringLiteral "Khiem likes ping pong" ))) } )))';
-      assert.equal(ast, expected);
-    });
-  });
-
-  describe('While loop', () => {
-    it('simple while', () => {
-      const ast = parse('while x == 5 { let y = 1 }').toString();
-      const expected = '(Program (Block (WhileStatement (BinaryExpression (IdExpression x) == (FloatLiteral 5.0)) { (Block (VariableDecl ((IdExpression y) = (FloatLiteral 1.0))) })))';
-      assert.equal(ast, expected);
-    });
-    it(' boolean while statement', () => {
-      const ast = parse('while true { if x == 5.0 { burp("HelloWorld") } }').toString();
-      const expected = '(Program (Block (WhileStatement (BooleanLiteral true) { (Block (IfStatement if (BinaryExpression (IdExpression x) == (FloatLiteral 5.0.0)) { (Block (Print burp (StringLiteral "HelloWorld" ))) } )) })))';
-// =======
+      const expected = '(Program (Block (ForStatement for (IdExpression beer) in (IdExpression beers) { (Block (Print burp (StringLiteral "Khiem likes ping pong"))) } )))';
       assert.equal(ast, expected);
     });
   });
@@ -134,36 +95,12 @@ describe('Parser Test', () => {
     it('boolean while statement', () => {
       const ast = parse('while true { if x == 5.0 { burp("HelloWorld") } }').toString();
       const expected = '(Program (Block (WhileStatement (BooleanLiteral true) { (Block (IfStatement if (BinaryExpression (IdExpression x) == (FloatLiteral 5.0.0)) { (Block (Print burp (StringLiteral "HelloWorld"))) } )) })))';
-// >>>>>>> a36ffaac9cb7d747952f5456998e814fa8c818be
       assert.equal(ast, expected);
     });
   });
 
-// <<<<<<< HEAD
-  describe('Constant Declaration', () => {
-    it('single constant decl', () => {
-      const ast = parse('set x = 5').toString();
-      const expected = '(Program (Block (ConstDecl ((IdExpression x) = (FloatLiteral 5.0))))';
-      assert.equal(ast, expected);
-    });
-    it('multiple constants decl', () => {
-      const ast = parse('set x,y = 5,6').toString();
-      const expected = '(Program (Block (ConstDecl ((IdExpression x) = (FloatLiteral 5.0) (IdExpression y) = (FloatLiteral 6.0)))))'
-    })
-  });
-
-  describe('Function Declaration', () => {
-    it('simple function declaration', () => {
-      const ast = parse('let someFunc = x = { burp(x) }').toString();
-      const expected = '(Program (Block (FunDecl)))';
-      assert.equal(ast, expected);
-    });
-  });
-
-// =======
 
   describe('ConstDecl', () => {
-<<<<<<< HEAD
       it('simple ConstDecl', () => {
         const ast = parse('set x = 5').toString();
         const expected = '(Program (Block (ConstDecl ((IdExpression x) = (FloatLiteral 5.0))))';
@@ -181,49 +118,5 @@ describe('Parser Test', () => {
           assert.equal(ast, expected);
         });
       });
-// >>>>>>> a36ffaac9cb7d747952f5456998e814fa8c818be
 
-=======
-    it('simple ConstDecl', () => {
-      const ast = parse('set x = 5').toString();
-      const expected = '(Program (Block (ConstDecl ((IdExpression x) = (FloatLiteral 5.0))))';
-      assert.equal(ast, expected);
-    });
-    it('multiple constants decl', () => {
-    const ast = parse('set x,y = 5,6').toString();
-    const expected = '(Program (Block (ConstDecl ((IdExpression x) = (FloatLiteral 5.0) (IdExpression y) = (FloatLiteral 6.0)))))'
-  });
-  });
-  describe('Drunken ConstDecl', () => {
-    it('types do not matter in declaration', () => {
-      const ast = parse('set beer, yup, fuzz = ["hi",[1,2],"you"], 15, "hey"').toString();
-      const expected = '(Program (Block (ConstDecl ((IdExpression beer) = (BoozieArray (StringLiteral "hi"),(BoozieArray (FloatLiteral 1.0),(FloatLiteral 2.0)),(StringLiteral "you"))),((IdExpression yup) = (FloatLiteral 15.0)),((IdExpression fuzz) = (StringLiteral "hey"))))';
-      assert.equal(ast, expected);
-    });
-  });
-  describe('FuncDecl', () => {
-    it('simple function', () => {
-      const ast = parse('let x = () => { burp("hey") }').toString();
-      const expected = '(Program (Block (FuncDecl (IdExpression x) = (Parameters ) => (Block (Print burp (StringLiteral "hey"))))))';
-      assert.equal(ast, expected);
-    });
-    it('multiple params', () => {
-      const ast = parse('let x = (dog, cat) => { burp(dog) }').toString();
-      const expected = '(Program (Block (FuncDecl (IdExpression x) = (Parameters (IdExpression dog),(IdExpression cat)) => (Block (Print burp (IdExpression dog))))))';
-      assert.equal(ast, expected);
-    });
-    it('Const FuncDecl', () => {
-      const ast = parse('set x = (dog, cat, jim) => { let x = 45 }').toString();
-      const expected = '(Program (Block (ConstFuncDecl (IdExpression x) = (Parameters (IdExpression dog),(IdExpression cat),(IdExpression jim)) => (Block (VariableDecl ((IdExpression x) = (FloatLiteral 45.0))))))';
-      assert.equal(ast, expected);
-    });
-  });
-  describe('Drunken ConstDecl', () => {
-    it('types do not matter in declaration', () => {
-      const ast = parse('set beer, yup, fuzz = ["hi",[1,2],"you"], 15, "hey"').toString();
-      const expected = '(Program (Block (ConstDecl ((IdExpression beer) = (BoozieArray (StringLiteral "hi"),(BoozieArray (FloatLiteral 1.0),(FloatLiteral 2.0)),(StringLiteral "you"))),((IdExpression yup) = (FloatLiteral 15.0)),((IdExpression fuzz) = (StringLiteral "hey"))))';
-      assert.equal(ast, expected);
-    });
-  });
->>>>>>> 4c0561c80b309df23079d81fbd37ec2f599da8c8
 });
